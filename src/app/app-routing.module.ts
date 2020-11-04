15854ea51ab2046add110a481619a4c641064a2d
Moath Kefayah <moath.kefayah@exalt.ps>
2020-11-04 15:37:29 +0200

    Add In-app Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // To make the app navigate to the dashboard automatically
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] // so it will be available throughout the app
 })
 export class AppRoutingModule { }
